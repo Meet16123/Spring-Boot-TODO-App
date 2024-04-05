9417ff73388df5a380457fc681f4f14230ffb8ad
Meet <meet16123@gmail.com>
2024-04-05 16:14:08 -0400

    First Commit

---
+++ main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(101,'Meet', 'Get AWS Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(102,'Striver', 'Get CP Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(103,'Harkirat', 'Get GCP Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(104,'Anuj', 'Learn DevOps', CURRENT_DATE(), false);
